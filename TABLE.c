450913aae5eb87d4e2e8e45591673dc419b3470d
PRAVEEN SINGH TOMAR <tomarpraveen640@gmail.com>
2025-04-13 22:17:39 +0530

    Update TABLE.c

--- TABLE.c
+++ TABLE.c
@@ -1,4 +1,11 @@
-int Table (int a,int i){
-        int tab = a * i ; 
-        return tab;
+#include<stdio.h>
+#include "Table.h"
+int main(){
+    int n;
+    printf("Input the No to get Table of : ");
+    scanf("%d",&n);
+    for(int i=1;i<=10;i++){
+       int b = Table(n,i);
+       printf("%d\n",b);
+    }
 }
